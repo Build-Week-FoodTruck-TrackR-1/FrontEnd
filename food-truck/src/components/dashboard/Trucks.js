1ca9d78aa2f72d58142ee1bd2fe80af6b40f2bce
George Davila <georgedavila@Georges-MacBook-Pro.local>
2020-02-29 19:40:00 -0500

    changed files and added new files

---
+++ food-truck/src/components/dashboard/Trucks.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import styled from 'styled-components';
+import standInImage from '../../images/delivery-truck-png-7.png'
+import { Typography, Button, Divider } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+const Container = styled.li`
+    display: flex;
+   
+    img {
+        flex-basis: 25%;
+        max-width: 33%;
+        max-height: 33%;
+        
+    }
+    div {
+        flex-basis: 75%;
+        display: flex;
+        flex-flow: column;
+        
+        p {
+            padding: 2.5%;
+        }
+        .MuiDivider-root {
+            height: 1%;
+            width: 95%;
+        }
+    }
+    #buttonDiv {
+        position: absolute;
+        
+        button {
+            float: right;
+            position: relative;
+            top: 10vh;
+            left: 320%;
+            width: 23vw;
+            span {
+                white-space: nowrap
+                
+            }
+        }
+    }
+    .MuiDivider-root {
+        height: 1%;
+        width: 100vw;
+    }
+`;
+
+const Truck = (props) => {
+
+
+
+    return (<Container>
+        <img src={standInImage}></img>
+
+        
+        <div>
+            <Typography noWrap={true}>{props.info.truckName}</Typography>
+            <Divider variant="middle"/>
+            <div>
+                <Typography>{`Serves: ${props.info.cuisineType}`}</Typography>
+                <Typography>{props.info.distance}</Typography>
+            </div>
+        </div>
+        <div id="buttonDiv">
+            <Link  to={`/truckpage/${props.info.id}`}>
+            <Button>View Menu</Button>
+            </Link>
+        </div>
+        
+    </Container>);
+};
+
+export default Truck;
---
+++ food-truck/src/components/dashboard/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+import DinerDashboardHeader from './dinerdashboardheader';
+import Map from './map';
+import TruckList from './trucklist';
+import MapInterface from './mapinterface';
+
+const Container = styled.section`
+    width: 100vw;
+`;
+
+const DinerDashboard = (props) => {
+
+    return (
+    <Container>
+        <DinerDashboardHeader/>
+        <MapInterface />
+        <Map />
+        <TruckList />
+
+    </Container>);
+};
+
+export default DinerDashboard;
