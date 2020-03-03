4f7c49d14e8d819aa4f2e9143a265122c26db5d0
George Davila <georgedavila@Georges-MacBook-Pro.local>
2020-03-03 18:59:28 -0500

    merged styling for forms

--- food-truck/src/components/Navbar.js
+++ food-truck/src/components/Navbar.js
@@ -1,23 +1,29 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-// import logo from "../images/logo-1.svg";
+import logo from "../images/truck-logo.png";
 import "../components/Styles/NavStyle.scss";
+import ScrollAnimation from "react-animate-on-scroll";
 
 const Navbar = () => {
   return (
     <header className="header-section">
-      <div className="logo-con">
-        {/* <img src={logo} alt="FoodTruck Logo" /> */}
-      </div>
+      <ScrollAnimation
+        offset={0}
+        animateIn="fadeInLeft"
+        animateOnce="true"
+        className="logo-con"
+      >
+        <img className="logo-img" src={logo} alt="FoodTruck Logo" />
+      </ScrollAnimation>
 
       <nav className="nav-bar">
-        {/* <a href="#">Home</a>
-        <a href="#">About Us</a> */}
+        <a href="#">Home</a>
+        <a href="#">About Us</a>
 
-        <NavLink to="/login" exact activeClassName="activeLink">
+        <NavLink to="/" exact activeClassName="activeLink">
           Login
         </NavLink>
-        <NavLink to="/" exact activeClassName="activeLink">
+        <NavLink to="/register" exact activeClassName="activeLink">
           Register
         </NavLink>
       </nav>
